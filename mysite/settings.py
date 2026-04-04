76ca2353f279fcb6eae60e15f86128c0b9b2e402
Paul-Gyan <gyanpaulkwasi@gmail.com>
2026-04-04 19:06:22 +0000

    Removed start.sh file

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ljw6d01ro@82vw2750cl3_p57%cy)!4gp=_t^34035dj6#!%ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-ltj.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition

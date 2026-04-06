228f949b4c35c71a4154c2113efc8d2c6613c301
Paul-Gyan <gyanpaulkwasi@gmail.com>
2026-04-06 13:58:15 +0000

    Added blog features

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,62 +1,70 @@
 from rest_framework import serializers
-from .models import Category, Like, Post, Comment, UserProfile, Category
-from taggit.serializers import (TagListSerializerField, TaggitSerializer)
+from .models import Category, Like, Post, Comment, UserProfile
+from taggit.serializers import TagListSerializerField, TaggitSerializer
 from django.contrib.auth.models import User
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'slug', 'description']
 
+
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
-        fields = ['id','post', 'author','author_name', 'content', 'date']
-        read_only_fields = ['author']
+        fields = ['id', 'post', 'author', 'author_name', 'content', 'date']
+        read_only_fields = ['author', 'post', 'author_name']
 
-    def get_author(self, obj):
-        return obj.author.username 
-    
+    def get_author_name(self, obj):
+        return obj.author.username
+
+
 class LikeSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Like
-        fields = ['id','post', 'user', 'date']
+        fields = ['id', 'post', 'user', 'date']
         read_only_fields = ['user']
+
 
 class UserProfileSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
-        fields = ['id', 'username', 'bio','avatar', 'location', 'website']
+        fields = ['id', 'username', 'bio', 'avatar', 'location', 'website']
 
     def get_username(self, obj):
         return obj.user.username
 
-   
-class PostSerializer(serializers.ModelSerializer):
+
+class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
     tags = TagListSerializerField()
     author_name = serializers.SerializerMethodField()
+    category_name = serializers.SerializerMethodField()
     total_likes = serializers.SerializerMethodField()
     total_comments = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'date', 'tags', 'author_name','author','image', 'total_likes', 'total_comments', 'comments']
+        fields = [
+            'id', 'title', 'content', 'date',
+            'author', 'author_name', 'category', 'category_name',
+            'tags', 'image', 'total_likes', 'total_comments', 'comments'
+        ]
         read_only_fields = ['author']
 
     def get_author_name(self, obj):
         return obj.author.username if obj.author else 'Anonymous'
-    
+
     def get_category_name(self, obj):
         return obj.category.name if obj.category else None
-    
+
     def get_total_likes(self, obj):
         return obj.total_likes()
-    
+
     def get_total_comments(self, obj):
         return obj.total_comments()
